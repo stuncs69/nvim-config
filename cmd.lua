40913a33f99fb691ef10787a849db677bbbe07e7
stuncs69 <68465860+stuncs69@users.noreply.github.com>
2023-02-08 17:13:34 +0100

    Add files via upload

---
+++ cmd.lua
@@ -0,0 +1,15 @@
+vim.cmd [[
+highlight NormalNC guibg=NONE
+colorscheme tender
+highlight Normal guibg=NONE
+highlight NonText guibg=NONE
+highlight SignColumn guibg=NONE
+filetype indent on
+filetype plugin indent on
+syntax on
+set number
+:cd
+set termguicolors
+syntax enable
+:hi NonText ctermbg=NONE guibg=NONE
+]]
---
+++ mapping.lua
@@ -0,0 +1,2 @@
+require("autoclose").setup({})
+vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTreeToggle<CR>", {silent = true})
