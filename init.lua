9c02362cd3c00803abe7f2a59cf52038d2e938fb
Tim Davids <68465860+stuncs69@users.noreply.github.com>
2023-02-08 14:36:56 +0100

    cleaning

--- init.lua
+++ init.lua
@@ -4,14 +4,16 @@ require('plugins.lualine')
 require('plugins.mapping')
 require('plugins.wilder')
 
-vim.cmd [[highlight NormalNC guibg=NONE]]
-vim.cmd [[colorscheme catppuccin-frappe]]
-vim.cmd [[highlight Normal guibg=NONE]]
-vim.cmd [[highlight NonText guibg=NONE]]
-vim.cmd [[highlight SignColumn guibg=NONE]]
-vim.cmd [[filetype indent on]]
-vim.cmd [[filetype plugin indent on]]
-vim.cmd [[syntax on]]
-vim.cmd [[set number]]
-vim.cmd [[:cd]]
-vim.cmd [[:hi NonText ctermbg=NONE guibg=NONE]]
+vim.cmd [[
+highlight NormalNC guibg=NONE
+colorscheme catppuccin-frappe
+highlight Normal guibg=NONE
+highlight NonText guibg=NONE
+highlight SignColumn guibg=NONE
+filetype indent on
+filetype plugin indent on
+syntax on
+set number
+:cd
+:hi NonText ctermbg=NONE guibg=NONE
+]]
