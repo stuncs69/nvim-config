c083fd8a77c8e9a489cebea4c54441b7b5549f30
Tim Davids <68465860+stuncs69@users.noreply.github.com>
2023-02-08 14:39:32 +0100

    true refactoring

--- init.lua
+++ init.lua
@@ -1,19 +1,6 @@
+require('plugins.cmd')
 require('plugins.plug')
 require('plugins.lsp')
 require('plugins.lualine')
 require('plugins.mapping')
 require('plugins.wilder')
-
-vim.cmd [[
-highlight NormalNC guibg=NONE
-colorscheme catppuccin-frappe
-highlight Normal guibg=NONE
-highlight NonText guibg=NONE
-highlight SignColumn guibg=NONE
-filetype indent on
-filetype plugin indent on
-syntax on
-set number
-:cd
-:hi NonText ctermbg=NONE guibg=NONE
-]]
---
+++ lua/plugins/cmd.lua
@@ -0,0 +1,13 @@
+vim.cmd [[
+highlight NormalNC guibg=NONE
+colorscheme catppuccin-frappe
+highlight Normal guibg=NONE
+highlight NonText guibg=NONE
+highlight SignColumn guibg=NONE
+filetype indent on
+filetype plugin indent on
+syntax on
+set number
+:cd
+:hi NonText ctermbg=NONE guibg=NONE
+]]
